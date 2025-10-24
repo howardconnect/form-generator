cee6d839d8f4323bdf8baa7a22ae15c7b6051cd6
Justin Howard <149806650+howardconnect@users.noreply.github.com>
2025-10-24 18:17:46 +0000

    Add JSON data files for objectives and snippets; update index.html for dynamic loading

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+const ovwSel = document.getElementById('overview').value;
+const bkgSel = document.getElementById('background').value;
+const objSel = document.getElementById('objectives').value;
+
+const ovw = resolveSnippet(ovwSel);
+const bkg = resolveSnippet(bkgSel);
+const obj = resolveObjectives(objSel);
+
+const overviewHtml = ovw ? mdToHtml(ovw.body_md) : "";
+const backgroundHtml = bkg ? mdToHtml(bkg.body_md) : "";
+const objectivesHtml = obj ? `<ul>${obj.items.map(i=>`<li>${i}</li>`).join('')}</ul>` : "";
